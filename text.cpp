1c21043182b44aa09f8e9a64b7c6978b007de88c
xxpppddd <118286404+xxpppddd@users.noreply.github.com>
2023-05-04 09:45:31 +0700

    Update text.cpp

--- text.cpp
+++ text.cpp
@@ -1,9 +1,3 @@
-//
-//  text.cpp
-//  tetris
-//
-//  Created by phuc dinh on 4/29/23.
-//
 
 #include "text.h"
 
@@ -37,3 +31,34 @@ void Text::renderText(SDL_Renderer* render, int x, int y)
 }
 
 
+#include "text.h"
+
+void Text::setFont(string filepath, int size)
+{
+    font = TTF_OpenFont(filepath.c_str(), size);
+
+}
+
+void Text::setText(string text, SDL_Renderer* render)
+{
+    if(text != this->text && text != "")
+    {
+        if(textTexture != NULL)
+            SDL_DestroyTexture(textTexture);
+        this->text = text;
+
+        SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), textColor);
+        textTexture = SDL_CreateTextureFromSurface(render, surface);
+        SDL_FreeSurface(surface);
+        SDL_QueryTexture(textTexture, NULL, NULL, &desRect.w, &desRect.h);
+    }
+
+}
+
+void Text::renderText(SDL_Renderer* render, int x, int y)
+{
+    desRect.x = x;
+    desRect.y = y;
+    SDL_RenderCopy(render, textTexture, NULL, &desRect);
+
+}
